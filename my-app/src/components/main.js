3a595d7952202ef7512af2d143c519332a530d2e
AlejandroCaleya <alejandrofcaleya@gmail.com>
2023-07-11 19:33:48 +0200

    working on btn show/hide

--- my-app/src/components/main.js
+++ my-app/src/components/main.js
@@ -2,15 +2,27 @@ import React from "react";
 import "../styles/main.css";
 
 const Main = () => {
+	var btn = document.getElementsByClassName("btnElegant");
+	btn.addEventListener("click", function () {
+		var vis = document.getElementsByClassName("Visibilidad");
+
+		if (vis.style.display == "block") {
+			vis.style.display = "none";
+		}
+	});
+
 	return (
 		<div className="main">
-			<h3>Encabezado de la pagina</h3>
-			<br></br>
-			<ol>
-				<li>Hola</li>
-				<li>Hola</li>
-				<li>Hola</li>
-			</ol>
+			<button className="btnElegant">Show/Hide</button>
+			<div className="Visibilidad">
+				<h3>Encabezado de la pagina</h3>
+				<br></br>
+				<ul>
+					<li>Hola</li>
+					<li>Hola</li>
+					<li>Hola</li>
+				</ul>
+			</div>
 		</div>
 	);
 };
